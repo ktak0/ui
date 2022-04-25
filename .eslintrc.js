f5946993e03666754b463c530af73fee8d4c3e1a
ktak <ktak.dev@gmail.com>
2022-04-25 22:22:09 +0900

    set version: "detect" to suppress eslint warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,11 @@ module.exports = {
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {

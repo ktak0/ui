2068059c1e11ebeae3b8abb5fdbbef7b9b647d7a
ktak <ktak.dev@gmail.com>
2022-04-26 00:19:24 +0900

    add storybook plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
+    "plugin:storybook/recommended",
   ],
   settings: {
     react: {
